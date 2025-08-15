aff0966dc735bf44dec54d312b9fcc6bc7b0e8ac
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-08-15 17:27:08 +0800

    feat: add new data entry functions

--- controller/indexController.js
+++ controller/indexController.js
@@ -8,7 +8,38 @@ async function getAllPokemonGet(req, res) {
     });
 };
 
+async function addNewPokemonGet(req, res) {
+    const trainers = await db.getAllTrainers();
+    const status = await db.getAllStatus();
+    const types = await db.getAllTypes();
+
+    res.render('newPokemon', {
+        title: 'New Pokemon',
+        trainers: trainers,
+        status: status,
+        types: types,
+    });
+};
+
+async function addNewPokemonPost(req, res) {
+    const { pokemon, nickname, trainer, status, type_one, type_two } = req.body;
+
+    const newPokemon = {
+        pokemon: pokemon,
+        nickname: nickname,
+        img_src: req.file ? `/uploads/${req.file.filename}` : '/images/Substitute_artwork.png',
+        trainer: trainer,
+        status: status,
+        type_one: type_one,
+        type_two: type_two,
+    };
+
+    await db.addPokemontoDb(newPokemon);
+    res.redirect('/');
+}
 
 module.exports = {
     getAllPokemonGet,
+    addNewPokemonGet,
+    addNewPokemonPost,
 }
