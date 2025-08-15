bc603bf9cc74333bdd1831110146f77a336bfbb2
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-08-15 17:29:56 +0800

    fix: uncomment lines of code; import middleware

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const indexController = require('../controller/indexController');
 const indexRouter = Router();
+const { upload } = require('../controller/uploadMiddleware');
 
 indexRouter.get('/', indexController.getAllPokemonGet);
-// indexRouter.get('/new', indexController.addNewPokemonGet);
-// indexRouter.post('/new', indexController.addNewPokemonPost);
+indexRouter.get('/new', indexController.addNewPokemonGet);
+indexRouter.post('/new', upload.single('image_src'), indexController.addNewPokemonPost);
 
 module.exports = indexRouter;
