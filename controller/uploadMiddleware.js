022a04ebe473ce3d278488c407a73fcf35914fbf
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-08-16 19:51:26 +0800

    feat: setup upload middleware

---
+++ controller/uploadMiddleware.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage, limits: { fileSize: 5000000 } });
+
+module.exports = {
+    upload,
+};
