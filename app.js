0fe194b08631b187e10b9ee02935ad7624965dba
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-08-18 18:36:25 +0800

    setup: uncomment routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 const assetsPath = path.join(__dirname, 'public');
 
 const indexRouter = require('./router/indexRouter');
-// const trainerRouter = require('./router/trainerRouter');
+const trainerRouter = require('./router/trainerRouter');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
 
 app.use('/', indexRouter);
-// app.use('/trainer', trainerRouter);
+app.use('/trainer', trainerRouter);
 
 const PORT = process.env.PORT || 3000;
 
