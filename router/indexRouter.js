2aecd0521232886d929e6b1ffdb1e426ea6c4c11
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-08-18 19:44:37 +0800

    setup: add route for deleting data

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -6,5 +6,6 @@ const { upload } = require('../controller/uploadMiddleware');
 indexRouter.get('/', indexController.getAllPokemonGet);
 indexRouter.get('/new', indexController.addNewPokemonGet);
 indexRouter.post('/new', upload.single('image_src'), indexController.addNewPokemonPost);
+indexRouter.post('/:id/delete', indexController.deletePokemonPost);
 
 module.exports = indexRouter;
