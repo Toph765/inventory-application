ec6d23457ead063e77f1b726ab62caf41e575609
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-08-20 17:11:27 +0800

    setup: add delete route

--- router/trainerRouter.js
+++ router/trainerRouter.js
@@ -5,5 +5,6 @@ const trainerRouter = Router();
 trainerRouter.get('/', trainerController.getAllTrainersGet);
 trainerRouter.get('/new', trainerController.addNewTrainerGet);
 trainerRouter.post('/new', trainerController.addNewTrainerPost);
+trainerRouter.post('/:id/delete', trainerController.deleteTrainerPost);
 
 module.exports = trainerRouter;
