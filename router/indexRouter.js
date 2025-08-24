0e780222dcd97cd414aa1a75852ab1d14fab8657
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-08-24 16:55:42 +0800

    setup: add route for edit

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -6,6 +6,8 @@ const { upload } = require('../controller/uploadMiddleware');
 indexRouter.get('/', indexController.getAllPokemonGet);
 indexRouter.get('/new', indexController.addNewPokemonGet);
 indexRouter.post('/new', upload.single('image_src'), indexController.addNewPokemonPost);
+indexRouter.get('/:id/edit', indexController.editPokemonGet);
+indexRouter.post('/:id/edit', upload.single('image_src'), indexController.editPokemonPost);
 indexRouter.post('/:id/delete', indexController.deletePokemonPost);
 
 module.exports = indexRouter;
