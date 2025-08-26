f31a53ba24d2847d419ee0de1e0ec5e14897cff5
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-08-26 18:49:01 +0800

    fix: remove get route

--- router/trainerRouter.js
+++ router/trainerRouter.js
@@ -3,7 +3,6 @@ const trainerController = require('../controller/trainerController');
 const trainerRouter = Router();
 
 trainerRouter.get('/', trainerController.getAllTrainersGet);
-trainerRouter.get('/new', trainerController.addNewTrainerGet);
 trainerRouter.post('/new', trainerController.addNewTrainerPost);
 trainerRouter.post('/:id/delete', trainerController.deleteTrainerPost);
 
